ef11f486401c21727d3af1cd056ff3e8c0edde74
hongkeattan <hongkeat.tan@torchminds.com>
2025-01-29 16:03:31 +0800

    Included custom module to fix pos sales details reporting decimal point issue

---
+++ tomo_pos_sales_details_decimal_fix/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- encoding: utf-8 -*-
+
+{
+    'name': 'Tomo POS Sales Details Decimal Fix',
+    'version': '18.0.0.1',
+    'description': "",
+    'author': 'Tomo',
+    'website': '',
+    'depends': ['point_of_sale'],
+    'data': [
+        'views/pos_session_sales_details.xml',
+    ],
+    'auto_install': False,
+    'application': True,
+    'license': 'LGPL-3',
+}
